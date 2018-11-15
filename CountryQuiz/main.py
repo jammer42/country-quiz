2c9b2645cd93c5c9a80389c3e688df5d9ad99ac0
James Mertens <jamesmertens42@gmail.com>
2018-11-15 19:22:43 +0000

    Initial commit of code

---
+++ CountryQuiz/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print('Country quiz')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CountryQuiz',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'CountryQuiz'},
+    url='https://github.com/jammer42/country-quiz',
+    license='MIT',
+    author='jammer42',
+    author_email='',
+    description=''
+)
